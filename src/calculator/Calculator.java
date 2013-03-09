a87c7891b66fa05adbe1618f57381f0f3717612c
Gio Magi <giomagi@gmail.com>
2013-03-09 19:30:22 +0000

    added calculation method (w/o implementation)

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,9 @@
+package calculator;
+
+import calculator.model.Taxpayer;
+
+public class Calculator {
+    public String taxpayerNumberFor(Taxpayer taxpayer) {
+        throw new RuntimeException("gio didn't implement me");
+    }
+}
--- test-acceptance/calculator/acceptance/test/CalculatorAcceptanceTest.java
+++ test-acceptance/calculator/acceptance/test/CalculatorAcceptanceTest.java
@@ -1,4 +1,5 @@
 package calculator.acceptance.test;
 
 public class CalculatorAcceptanceTest {
+
 }
